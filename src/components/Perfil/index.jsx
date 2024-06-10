488a2b8ddb810029ea929112d397d8d219b53f8c
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-06-10 01:28:11 -0300

    Explorando CSS modules

--- src/components/Perfil/index.jsx
+++ src/components/Perfil/index.jsx
@@ -3,8 +3,8 @@ import styles from './Perfil.module.css';
 const Perfil = ({ nomeUsuario }) => {
     return (
         <header className={styles.header}>
-            <img className="perfil-avatar" src={`https://github.com/${nomeUsuario}.png`} />
-            <h1 className="perfil-titulo">
+            <img className={styles.avatar} src={`https://github.com/${nomeUsuario}.png`} />
+            <h1 className={styles.name}>
                 {nomeUsuario}
             </h1>
         </header>
--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 
-const ReposList = () => {
+import styles from './ReposList.module.css';
+
+const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
 
     useEffect(() => {
-        fetch('https://api.github.com/users/RodCinelli/repos')
+        setEstaCarregando(true);
+        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
             .then(res => res.json())
             .then(resJson => {
                 setTimeout(() => {
@@ -13,24 +16,32 @@ const ReposList = () => {
                     setRepos(resJson);
                 }, 3000);
             })
-    }, []);
+    }, [nomeUsuario]);
 
     return (
-        <>
-            {estaCarregando && (
+        <div className="container">
+            {estaCarregando ? (
                 <h4>Carregando...</h4>
+            ) : (
+
+                <ul className={styles.list}>
+                    {/* {repos.map(repositorio => ( */}
+                    {repos.map(({ id, name, language, html_url }) => (
+                        <li className={styles.listItem} key={id}>
+                            <div className={styles.itemName}>
+                                <b>Nome:</b>
+                                {name}
+                            </div>
+                            <div className={styles.itemLanguage}>
+                                <b>Linguagem:</b>
+                                {language}
+                            </div>
+                            <a className={styles.itemLink} target="_blank" href={html_url}>Visitar no Github</a>
+                        </li>
+                    ))}
+                </ul>
             )}
-            <ul>
-                {/* {repos.map(repositorio => ( */}
-                {repos.map(({ id, name, language, html_url }) => (
-                    <li key={id}>
-                        <b>Nome:</b> {name} <br />
-                        <b>Linguagem:</b> {language} <br />
-                        <a target="_blank" href={html_url}>Visitar no Github</a> <br />
-                    </li>
-                ))}
-            </ul>
-        </>
+        </div>
     )
 }
 
